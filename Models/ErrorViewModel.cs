3106323c39d5f10a3cc2ca72f3d7554a1d37e15e
Deepak PG <deepakpg121@gmail.com>
2023-10-09 20:27:33 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
