6d96135b23ffe243f2f819a85a08a406d38cb58a
Deepak PG <deepakpg121@gmail.com>
2023-10-11 20:36:22 +0530

    Define the UserDetails model for user data storage

---
+++ Models/UserDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace mvc_core.Models;
+
+public partial class UserDetails
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Course { get; set; }
+
+    public string? State { get; set; }
+}
