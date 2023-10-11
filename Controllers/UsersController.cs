c1458c6e8266d998abad02541b472e735aa8045e
Deepak PG <deepakpg121@gmail.com>
2023-10-11 20:39:29 +0530

    Implement CRUD operations in UsersController for managing UserDetails

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mvc_core.Models;
+
+namespace mvc_core.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly CoreContext _context;
+
+        public UsersController(CoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+              return _context.MvcCores != null ? 
+                          View(await _context.MvcCores.ToListAsync()) :
+                          Problem("Entity set 'CoreContext.MvcCores'  is null.");
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.MvcCores == null)
+            {
+                return NotFound();
+            }
+
+            var userDetails = await _context.MvcCores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(userDetails);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Age,Gender,Email,Course,State")] UserDetails userDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userDetails);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userDetails);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.MvcCores == null)
+            {
+                return NotFound();
+            }
+
+            var userDetails = await _context.MvcCores.FindAsync(id);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+            return View(userDetails);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Age,Gender,Email,Course,State")] UserDetails userDetails)
+        {
+            if (id != userDetails.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userDetails);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserDetailsExists(userDetails.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userDetails);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.MvcCores == null)
+            {
+                return NotFound();
+            }
+
+            var userDetails = await _context.MvcCores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(userDetails);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.MvcCores == null)
+            {
+                return Problem("Entity set 'CoreContext.MvcCores'  is null.");
+            }
+            var userDetails = await _context.MvcCores.FindAsync(id);
+            if (userDetails != null)
+            {
+                _context.MvcCores.Remove(userDetails);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserDetailsExists(int id)
+        {
+          return (_context.MvcCores?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
