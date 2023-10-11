1cab41384ba37606eedfece54d592a4844f961f4
Deepak PG <deepakpg121@gmail.com>
2023-10-11 20:38:02 +0530

    Comment out connection string warning message

---
+++ Models/CoreContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mvc_core.Models;
+
+public partial class CoreContext : DbContext
+{
+    public CoreContext()
+    {
+    }
+
+    public CoreContext(DbContextOptions<CoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserDetails> MvcCores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-PBB3G4S;Database=core;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserDetails>(entity =>
+        {
+            entity.ToTable("MvcCore");
+
+            entity.Property(e => e.Course)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Gender).HasMaxLength(50);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
